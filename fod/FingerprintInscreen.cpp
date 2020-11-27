a4a69ccedde15761cde6d7eeabb9e1ab444b9da7
alibei <sebastiankotzias83@gmail.com>
2020-11-28 00:51:25 +0100

    tucana: Update fod sensor position

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -31,7 +31,7 @@
 #define FOD_STATUS_OFF 0
 
 #define FOD_SENSOR_X 445
-#define FOD_SENSOR_Y 1931
+#define FOD_SENSOR_Y 1715
 #define FOD_SENSOR_SIZE 190
 
 #define BRIGHTNESS_PATH "/sys/class/backlight/panel0-backlight/brightness_clone"
