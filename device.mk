1f0a2aec3d64222acec15c1f8af67c0870747005
alibei <sebastiankotzias83@gmail.com>
2020-11-30 08:54:29 +0100

    Revert "tucana: Switch to passthrough light hal"
    This reverts commit 776a03dfbc98c9c45aa005def4f5b5e8401e772c.

--- device.mk
+++ device.mk
@@ -90,7 +90,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl.tucana
+    android.hardware.light@2.0-service.tucana
 
 # NFC
 PRODUCT_PACKAGES += \
